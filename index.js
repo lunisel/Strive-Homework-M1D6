6b159fa515e812886c488453d74dfca24e163bba
lunisel <selene942008@hotmail.com>
2021-05-24 10:21:19 +0200

    Exercise A and B done

--- index.js
+++ index.js
@@ -16,9 +16,15 @@
    Create a variable called "test" and assign a string to it.
 */
 
+let test = "This is a test"
+console.log(test)
+
 /* Ex.B
     Create a variable called "sum" and assign to it the result of the sum between 10 and 20.
 */
+
+let sum = 10*20
+console.log(sum)
 
 /* Ex.C 
     Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
