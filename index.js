aa9456f4a010ba3dc82cbebe8f031de1d8306fae
lunisel <selene942008@hotmail.com>
2021-05-24 10:39:30 +0200

    Ex. F done

--- index.js
+++ index.js
@@ -56,6 +56,9 @@ console.log(me)
    Programmatically add to the object me an array called "skills", containing the programming languages you know right now.
 */
 
+me.skills = ["JavaScript"]
+console.log(me)
+
 /* Ex.G 
    Programmatically remove the last skill from the "skills" array inside the "me" object.
 */
