ccb83880bfbd90098a74c048a7fed1d1495b25bc
lunisel <selene942008@hotmail.com>
2021-05-24 10:32:03 +0200

    Ex. E done

--- index.js
+++ index.js
@@ -49,6 +49,9 @@ console.log(me)
     Programmatically remove the age property from the previously create object.
 */
 
+delete me.age
+console.log(me)
+
 /* Ex.F 
    Programmatically add to the object me an array called "skills", containing the programming languages you know right now.
 */
